f9a82583ba7033e89531444ca5015ad16618cb33
detorresramos <detorresramos1@gmail.com>
2020-01-18 14:00:34 -0600

    reformatted data to use

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ import Selection from "./components/Selection";
 import ControlPanel from "./components/ControlPanel";
 import Submission from "./components/Submission";
 
+import data from "./data/CourseData.json";
+
 function App() {
+  
+  // COMP -> 140 -> Fall 2019 -> Rixner
+  var courseToNumbers = {}
+
+  for (let key in data) {
+    let [subject, number] = key.split(" ");
+    if (!(subject in courseToNumbers)) {
+      courseToNumbers[subject] = {}
+    }
+    if (!(number in courseToNumbers[subject])) {
+      courseToNumbers[subject][number] = {}
+    }
+    
+    for (let term in data[key]) {
+      if (!(term in courseToNumbers[subject][number])) {
+        courseToNumbers[subject][number][term] = []
+      }
+      for (let crnObject of data[key][term]) {
+        courseToNumbers[subject][number][term].push(crnObject["crn"]);
+      }
+    }
+  }
+
+  console.log(courseToNumbers);
+
   return (
     <div className="App">
       <Title />
