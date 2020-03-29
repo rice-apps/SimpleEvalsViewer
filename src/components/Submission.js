3a81003565502164ed347dc62c49cb86d4193409
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-03-29 17:28:58 -0400

    adjusted height of iframe for less confusion

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -5,7 +5,7 @@ function Submission({CRN, term, show }) {
   if (show) {
     const url = `https://esther.rice.edu/selfserve/swkscmt.main?p_term=${term}&p_crn=${CRN}&p_commentid=&p_confirm=1&p_type=Course`;
     return (
-        <div className="Submission" style={{ height: "100vh" }}>
+        <div className="Submission" style={{ height: "75vh" }}>
             <iframe height="100%" width="100%" title="iframe1" src={url}/>
         </div>
     );
